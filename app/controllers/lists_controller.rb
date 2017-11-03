b49d3be5e88f70020a616e48a0bbb024be51927b
Kurkulya <kulinich_v@hotmail.com>
2017-11-03 19:47:42 +0200

    Fixed tasks

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,12 +4,13 @@ class ListsController < ApplicationController
   # GET /lists
   # GET /lists.json
   def index
-    @lists = current_user.lists
+    #@lists = current_user.lists
   end
 
   # GET /lists/1
   # GET /lists/1.json
   def show
+    #@list = List.find(params[:id])
   end
 
   # GET /lists/new
