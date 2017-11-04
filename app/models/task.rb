aec05dc190aa12b9ad171dad490779ce27b499ce
Kurkulya <kulinich_v@hotmail.com>
2017-11-04 13:06:14 +0200

    Fixed psitions moving

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,9 +6,11 @@ class Task < ApplicationRecord
 
   def swap_positions(pos)
     temp_pos = self.position
-    Task.find_by_position(pos).update(position: temp_pos)
+    List.find(self.list_id).tasks.find_by_position(pos).update(position: temp_pos)
     self.update(position: pos)
   end
+
+
 
   def update_positions
     List.find(self.list_id).tasks.where("position > ?", self.position).each do |t|
