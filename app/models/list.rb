c73f6d3ea9bf41b43ab124d2520e97af2614bd8f
Kurkulya <kulinich_v@hotmail.com>
2017-11-07 14:41:22 +0200

    updated gitignore deleted idea

--- app/models/list.rb
+++
@@ -1,5 +0,0 @@
-class List < ApplicationRecord
-  belongs_to :user
-
-  has_many :tasks, dependent: :destroy
-end
--- app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ActiveRecord::Base
-  # Include default devise modules.
-  devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable,
-          :confirmable
-  include DeviseTokenAuth::Concerns::User
-
-  has_many :lists, dependent: :destroy
-end
